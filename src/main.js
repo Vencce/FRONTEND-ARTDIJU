dd0bf723ffc81a1e7ddb6b369a8024db2a1b219f
vencce <vitorferreira20005@gmial.com>
2026-07-10 11:48:46 -0300

    refactor: remove VueGtag integration from main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -11,9 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.use(VueGtag, {
-  config: { id: 'GTM-M5ZRQ4QQ' }
-}, router)
 
 app.mount('#app')
