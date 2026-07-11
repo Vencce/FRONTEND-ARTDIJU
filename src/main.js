75433df6ad56e9e7ac5eb6d4c90c61badb03f3c4
vencce <vitorferreira20005@gmial.com>
2026-07-10 23:31:21 -0300

    refactor: update Google Tag Manager integration in index.html and remove VueGtag from main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueGtag from 'vue-gtag'
 
 import App from './App.vue'
 import router from './router'
@@ -11,9 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.use(VueGtag, {
-  config: { id: 'G-538GDEBMV5' }
-}, router)
 
 app.mount('#app')
